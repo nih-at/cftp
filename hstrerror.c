1e443484e17df0ccbe05498624dafdb51df9f759
Dieter Baron <dillo@nih.at>
1996-12-21 16:18:50 +0000

    added copyright notice

--- hstrerror.c
+++ hstrerror.c
@@ -1,18 +1,40 @@
-char    *h_errlist[] = {
-	"Error 0",
-	"Unknown host",                         /* 1
-						   HOST_NOT_FOUND */
-	"Host name lookup failure",             /* 2
-						   TRY_AGAIN */
-	"Unknown server error",                 /* 3
-						   NO_RECOVERY */
-	"No address associated with name",      /* 4
-						   NO_ADDRESS */
-	"Service unavailable",			/* 5
-						   AIX: SERVICE_UNAVAILABLE */
+/*
+  hstrerror -- get resolver error message string
+  Copyright (C) 1996 Dieter Baron
+
+  The author can be contacted at <dillo@giga.or.at>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+
+#include <stdio.h>
+
+char *h_errlist[] = {
+    "Error 0",
+    "Unknown host",			/* 1 HOST_NOT_FOUND */
+    "Host name lookup failure",		/* 2 TRY_AGAIN */
+    "Unknown server error",		/* 3: NO_RECOVERY */
+    "No address associated with name",	/* 4: NO_ADDRESS */
+    "Service unavailable",		/* 5 AIX: SERVICE_UNAVAILABLE */
 };
 
-int     h_nerr = sizeof(h_errlist)/sizeof(h_errlist[0]);
+int h_nerr = sizeof(h_errlist)/sizeof(h_errlist[0]);
+
+
 
 char *
 hstrerror(int err)
