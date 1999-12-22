ce22b15aec321976e194f04519b77762d1fd0526
Dieter Baron <dillo@nih.at>
1999-12-22 17:00:33 +0000

    HAVE_SCRIPTING

--- acconfig.h
+++ acconfig.h
@@ -3,3 +3,9 @@
 
 /* Define if guile support should be compiled */
 #undef HAVE_GUILE
+
+@BOTTOM@
+/* Define if we have any scripting language */
+#if defined(HAVE_GUILE)
+#define HAVE_SCRIPTING
+#endif
