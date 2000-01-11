d01f776b461d61287e473b27bc3f2fc1be8faf8e
Dieter Baron <dillo@nih.at>
2000-01-11 16:04:26 +0000

    guile hacking

--- fn_guile.c
+++ fn_guile.c
@@ -6,7 +6,11 @@
 
 #include "display.h"
 
-char *scm2cstring(SCM object, int *length_p);
+static char *scm2cstring(SCM object, int *length_p);
+static SCM execute_guile(char *s);
+
+static char guile_errstr[8192];
+static int guile_error;
 
 
 
@@ -21,19 +25,24 @@ void fn_guile(char **args)
     else
 	cmd = read_string("<guile>", 1);
 
-    ret = gh_eval_str(cmd);
-
+    guile_error = 0;
+    ret = execute_guile(cmd);
+    
     if (!args)
 	free(cmd);
 
-    cmd = scm2cstring(ret, &len);
-    disp_status("%s", cmd);
-    free(cmd);
+    if (guile_error)
+	disp_status("%s", guile_errstr);
+    else {
+	cmd = scm2cstring(ret, &len);
+	disp_status("%s", cmd);
+	free(cmd);
+    }
 }
 
 
 
-char *
+static char *
 scm2cstring(SCM object, int *length_p)
 {
     SCM stringify;
@@ -48,29 +57,27 @@ scm2cstring(SCM object, int *length_p)
 
 
 
-#if 0
 static SCM wrapper(void *data, SCM jmpbuf)
 {
-  char *scheme_code = (char *)data;
-  SCM res = gh_eval_str(scheme_code);
-  return res;
+    char *scheme_code = (char *)data;
+    SCM res = gh_eval_str(scheme_code);
+    return res;
 }
 
 static SCM catcher(void *data, SCM tag, SCM throw_args)
 {
-	char b[256];
-
-	strcpy(b, "ERROR: ");
-	strncat(b, (char *)data, 200);
-	return SCM_BOOL_F;
+    strcpy(guile_errstr, "ERROR: ");
+    strncat(guile_errstr, (char *)data, 8180);
+    guile_error = 1;
+    
+    return SCM_BOOL_F;
 }
 
-void execute_guile(char *s)
+static SCM execute_guile(char *s)
 {
-	gh_catch(SCM_BOOL_T,
-			(scm_catch_body_t)wrapper, s,
-			(scm_catch_handler_t)catcher, s);
+    return gh_catch(SCM_BOOL_T,
+		    (scm_catch_body_t)wrapper, s,
+		    (scm_catch_handler_t)catcher, s);
 }
-#endif
 
 #endif
