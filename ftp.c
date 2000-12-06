e36586535fa3f0db8d91a09db9b77e482db88dce
Dieter Baron <dillo@nih.at>
2000-12-06 01:51:54 +0000

    rename variable s_addr, as that is a macro under Solaris

--- ftp.c
+++ ftp.c
@@ -696,7 +696,7 @@ ftp_port(void)
     unsigned long host;
     int fd, port, val, i, delim;
     int len;
-    unsigned int s_addr;
+    unsigned int iaddr;
     unsigned char addr[4];
     char baddr[16], bport[6], *s, *e;
     char *saddr, *sport;
@@ -718,10 +718,10 @@ ftp_port(void)
 	port = htons(((struct sockaddr_in *)&sa)->sin_port);
 
 	if (ftp_addr->sa_family == AF_INET) {
-	    s_addr = htonl(sin->sin_addr.s_addr);
+	    iaddr = htonl(sin->sin_addr.s_addr);
 	    ftp_put("port %d,%d,%d,%d,%d,%d",
-		    (s_addr>>24) & 0xff, (s_addr>>16) & 0xff,
-		    (s_addr>>8) & 0xff, s_addr & 0xff,
+		    (iaddr>>24) & 0xff, (iaddr>>16) & 0xff,
+		    (iaddr>>8) & 0xff, iaddr & 0xff,
 		    (port>>8) & 0xff, port & 0xff);
 	}
 	else {
